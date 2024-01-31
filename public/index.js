fe4cff0113f2b4f1fc5229238675a5f93d70f4e8
davit <torgomyandavid96@gmail.com>
2024-01-31 17:00:38 +0400

    chnage compose bind command

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@
     const selectElement = document.getElementById("accountType");
     // fetch("http://localhost:8000/banks")
 
-    fetch("http://38.242.152.121:8000/banks")
+    fetch("http://vmi1512737:8000/banks")
     .then(response => {
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
